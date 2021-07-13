8b9fb32b12310040355dc5469af23dad95f21dca
sin-youngjin <nmrhtn7898@naver.com>
2021-07-13 23:05:18 +0900

    add resthighlevelclient credentials

--- src/main/java/com/nuguri/springbatchexample/SpringBatchExampleApplication.java
+++ src/main/java/com/nuguri/springbatchexample/SpringBatchExampleApplication.java
@@ -3,6 +3,11 @@ package com.nuguri.springbatchexample;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -25,7 +30,11 @@ public class SpringBatchExampleApplication {
 
     @Bean
     public RestHighLevelClient restHighLevelClient() {
-        RestClientBuilder restClientBuilder = RestClient.builder(HttpHost.create("http://localhost:9200"));
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("elastic", "changeme"));
+        RestClientBuilder restClientBuilder = RestClient
+                .builder(HttpHost.create("http://localhost:9200"))
+                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
         return new RestHighLevelClient(restClientBuilder);
     }
 
